6f7649c3ee14c520913ada9b161c4c8af09d92dc
Chauncey2 <gaochangju95@163.com>
2023-03-02 23:04:57 +0800

    字符串-替换空格字符串

---
+++ 字符串/替换空格.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   替换空格.py
+@Time    :   2023/03/02 22:48:29
+@Author  :   gaochangju
+@Version :   1.0
+@Desc    :   None
+'''
+
+"""
+请实现一个函数，把字符串 s 中的每个空格替换成"%20"
+例如：
+s = "We are happy."
+输出："We%20are%20happy."
+"""
+# 双指针的用法
+def function(s:str)->str:
+    pass
+
+# here put the import lib
+if __name__ == "__main__":
+    pass
