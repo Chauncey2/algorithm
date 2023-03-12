2ca121a7c929ab930635c4c89fd016e97e7c1846
gaochangju <gaochangju@datagrand.com>
2023-03-12 16:56:38 +0800

    滑动窗口最大值

--- 栈和队列/滑动窗口最大值.py
+++ 栈和队列/滑动窗口最大值.py
@@ -16,9 +16,16 @@ class myQueue:
         self.queue = deque()
     
     def push(self,val):
+        # 窗口队列只需要保证每队首的元素最大
+        # 比较的方法是从队尾依次比较，这一步骤是为了保证队列有序（降序）
+        while self.queue and val > self.queue[-1]:
+            self.queue.pop()
+        self.queue.append(val)
+    
+    def pop(self,val):
+        # 如果滑动窗口移除的值比队首的小，则不需要做任何操作
         pass
-    def pop(self):
-        pass
+    
     def front(self):
         # 返回当前chuang口的最大值
         pass
