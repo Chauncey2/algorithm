3953e02ea2c649d20af448dd654429c17a55029a
gaochangju <gaochangju@datagrand.com>
2023-03-12 16:41:31 +0800

    滑动窗口最大值

---
+++ 栈和队列/滑动窗口最大值.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   滑动窗口最大值.py
+@Time    :   2023/03/12 16:10:01
+@Author  :   gaochangju 
+@Version :   1.0
+'''
+
+# here put the import lib
+from collections import deque
+
+
+class myQueue:
+    def __init__(self):
+        self.queue = deque()
+    
+    def push(self,val):
+        pass
+    def pop(self):
+        pass
+    def front(self):
+        # 返回当前chuang口的最大值
+        pass
+
+def function(nums:list):
+    pass
+    
+
+
+if __name__ == "__main__":
+    pass
