6d638b2f4cdad7d5df0c38037be4a8a44735b760
gaochangju <gaochangju@datagrand.com>
2023-03-16 11:09:28 +0800

    栈和队列:前k个高频元素

--- 栈和队列/前k个高频元素.py
+++ 栈和队列/前k个高频元素.py
@@ -27,9 +27,30 @@
 你可以按任意顺序返回答案。
 
 """
-def function():
-    pass
+import heapq
+
+def function(nums:list[int],k:int):
+    # 统计元素出现的频率
+    map_ = {}
+    for i in range(len(nums)):
+        map_[nums[i]] = map_.get(nums[i],0) + 1
+        
+    # 用固定大小为k的小顶堆,扫描所有元素的数值
+    heaq_list = []
+    for item,freq in map_.items():
+        heapq.heappush(heaq_list,(freq,item))
+        if len(heaq_list) > k:
+            heapq.heappop(heaq_list)
+    # 因为是小顶堆，所以倒序输出,
+    print(heaq_list)
+    result = [0]*k
+    # range(start,stop[,step]) stop的值不会访问，所以在做倒序输出的时候是要注意（我总结这种特性叫 顾头不顾腚）
+    for i in range(k-1,-1,-1):
+        result[i] = heapq.heappop(heaq_list)[1]
  
 
 if __name__ == "__main__":
-    pass
+    nums = [1,1,1,2,2,3]
+    k = 2
+    function(nums=nums,k=k)
+    
