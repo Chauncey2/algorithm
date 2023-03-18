dcd57f76648dd2c8c08fdda54856f4c91cb35ac9
gaochangju <gaochangju@datagrand.com>
2023-03-18 13:37:52 +0800

    二叉树：二叉树的最大深度+ 修改层序遍历

--- 二叉树/二叉树的层序遍历.py
+++ 二叉树/二叉树的层序遍历.py
@@ -15,18 +15,38 @@ class BinaryTreelevleOrder(BinaryTree):
     def __init__(self, nodes:List[TreeNode]) -> None:
         super().__init__(nodes)
 
-    def level_order(self, root: TreeNode) -> list:
+    # def level_order(self, root: TreeNode) -> list:
+    #     if root is None:
+    #         return []
+
+    #     queue = []
+    #     queue.append(root)
+    #     result = []
+    #     while queue:
+    #         node = queue.pop(0)  # 出队首元素
+    #         result.append(node.value)
+    #         if node.left:
+    #             queue.append(node.left)
+    #         if node.right:
+    #             queue.append(node.right)
+    #     return result
+
+    def level_order(self,root:TreeNode)->list:
+        # 这种方法其实是n叉树的处理方法
         if root is None:
             return []
-
-        queue = []
-        queue.append(root)
+        from collections import deque
+        que = deque()
+        que.append(root)
         result = []
-        while queue:
-            node = queue.pop(0)  # 出队首元素
-            result.append(node.value)
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
+        while len(que):
+            size = len(que)
+            for _ in range(size):
+                node = que.popleft()
+                result.append(node.value)
+                if node.left:
+                    que.append(node.left)
+                if node.right:
+                    que.append(node.right)
         return result
+            
