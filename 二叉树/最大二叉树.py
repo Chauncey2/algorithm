10c46f78e2329097c0ff1a1aab1704a7a076dca3
gaochangju <gaochangju@datagrand.com>
2023-03-22 10:03:28 +0800

    二叉树：递归法构造最大二叉树

--- 二叉树/最大二叉树.py
+++ 二叉树/最大二叉树.py
@@ -13,7 +13,7 @@
 二叉树的根是数组中的最大元素。
 左子树是通过数组中最大值左边部分构造出的最大二叉树。
 右子树是通过数组中最大值右边部分构造出的最大二叉树。
-通过给定的数组构建最大二叉树，并且输出这个树的根节点。
+通过给定的数组构建最大二叉树，并且输 出这个树的根节点。
 """
 # here put the import lib
 from typing import List
@@ -21,5 +21,30 @@ from . import TreeNode
 
 class Solution:
     def constructMaximumBinaryTree(self,nums:List[int])-> TreeNode:
-        # 使用递归法构建构建二叉树
-        pass
+        """ 使用递归构造最大二叉树
+
+        Args:
+            nums (List[int]): 数组
+
+        Returns:
+            TreeNode: 根节点
+        """
+        # 如果数组为空则返回空节点
+        if len(nums):
+            return None
+
+        # 找到当前数组中的最大值并切割
+        max_value = max(nums)
+        root = TreeNode(max_value)
+        max_index = nums.index(max_value)
+        # 切割
+        left_nums = nums[:max_index]
+        right_nums = nums[max_index+1:]
+        
+        # 递归
+        root.left = self.constructMaximumBinaryTree(left_nums)
+        root.right = self.constructMaximumBinaryTree(right_nums)
+        # 返回值
+        return root
+        
+        
