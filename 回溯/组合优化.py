4e4237f978dbb71ad7c911f720d653c38ff653b3
gaochangju <gaochangju@datagrand.com>
2023-03-26 12:33:20 +0800

    回溯：递归剪枝存疑？

---
+++ 回溯/组合优化.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   组合优化.py
+@Time    :   2023/03/26 12:17:17
+@Author  :   gaochangju 
+@Version :   1.0
+'''
+
+# here put the import lib
+# 回溯中的剪枝操作如何理解 
